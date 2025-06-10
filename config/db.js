ba20dc01fc8c0f7944789cccd09a2f1c44db5203
“Matias <124017375+maticorvalan@users.noreply.github.com>
2025-06-10 04:21:55 -0300

    Update en la db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,17 @@
 // models/db.js
 import { Sequelize } from "sequelize";
 import 'dotenv/config'
+import mysql2 from 'mysql2';
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'mysql',
-  logging: false
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2,
+    logging: false
 });
 
 export default sequelize;
